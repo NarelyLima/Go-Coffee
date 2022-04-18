f124572a944528271d0b2e6b28cefc0265298cb1
NarelyLima <narely148@gmail.com>
2022-04-18 11:06:54 -0300

    Initial Commit

---
+++ FruitKu/FruitKuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitKuApp.swift
+//  FruitKu
+//
+//  Created by Narely Lima on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitKuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
